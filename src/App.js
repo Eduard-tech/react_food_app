1ccef9b05bd8b54b1200ed28022918dfc06a1d38
Eduard Ciugulea <ciugulea@yahoo.com>
2021-07-12 10:51:14 +0100

    Modal working

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
+import { useState } from "react";
 
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+
+  const showCarHandler = () => {
+    setCartIsShown(true);
+  }
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  }
+
   return (
     <>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler}/>}
+      <Header onShowCart={showCarHandler} />
       <main>
         <Meals />
       </main>
